dc5bae9b73b6a131cb52f93d77dd725e2b73a09f
Nick DeGroot <1966472+nickthegroot@users.noreply.github.com>
2023-03-09 03:33:30 +0000

    RecLGN Model

--- src/models/conv.py
+++ src/models/conv.py
@@ -1,7 +1,7 @@
 import torch
 from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.typing import Adj, OptTensor
-from torch_scatter import scatter_add
+from torch_geometric.utils import degree
 
 
 class HeteroLGConv(MessagePassing):
@@ -12,9 +12,8 @@ class HeteroLGConv(MessagePassing):
     based on a weighted sum of its neighbors' features.
     """
 
-    def __init__(self, normalize: bool = True):
+    def __init__(self):
         super().__init__(aggr="add")
-        self.normalize = normalize
 
     def forward(
         self,
@@ -22,30 +21,19 @@ class HeteroLGConv(MessagePassing):
         edge_index: Adj,
         edge_weight: OptTensor = None,
     ):
-        in_x, out_x = x
-        if self.normalize:
-            if edge_weight is None:
-                edge_weight = torch.ones(
-                    (edge_index.size(1),), dtype=in_x.dtype, device=in_x.device
-                )
-
-            num_in_nodes = in_x.size(0)
-            num_out_nodes = out_x.size(0)
-
-            in_idx, out_idx = edge_index[0], edge_index[1]
-            in_deg = scatter_add(edge_weight, in_idx, dim=0, dim_size=num_in_nodes)
-            in_deg_inv_sqrt = in_deg.pow(-0.5)
-            in_deg_inv_sqrt.masked_fill_(in_deg_inv_sqrt == float("inf"), 0)
-
-            out_deg = scatter_add(edge_weight, out_idx, dim=0, dim_size=num_out_nodes)
-            out_deg_inv_sqrt = out_deg.pow(-0.5)
-            out_deg_inv_sqrt.masked_fill_(out_deg_inv_sqrt == float("inf"), 0)
-
-            edge_weight = (
-                in_deg_inv_sqrt[in_idx] * edge_weight * out_deg_inv_sqrt[out_idx]
+        if edge_weight is None:
+            edge_weight = torch.ones(
+                edge_index.size(1),
+                dtype=x[0].dtype,
+                device=x[0].device,
             )
 
-        return self.propagate(edge_index, x=x, edge_weight=edge_weight, size=None)
+        in_idx, out_idx = edge_index[0], edge_index[1]
+        in_deg, out_deg = degree(in_idx), degree(out_idx)
+        norm_term = (in_deg[in_idx] * out_deg[out_idx]).pow(-0.5)
+
+        new_edge_weight = edge_weight * norm_term
+        return self.propagate(edge_index, x=x, edge_weight=new_edge_weight, size=None)
 
     def message(self, x_j: torch.Tensor, edge_weight: OptTensor):
-        return x_j if edge_weight is None else edge_weight.view(-1, 1) * x_j
+        return edge_weight.view(-1, 1) * x_j
--- src/models/heterolgn.py
+++ src/models/heterolgn.py
@@ -21,10 +21,10 @@ class HeteroLGN(nn.Module):
         self.num_users = num_users
         self.num_recipes = num_recipes
         self.usr_embedding = nn.Embedding(num_users, embedding_dim)
-        self.rec_embedding = nn.Embedding(num_recipes, embedding_dim)
+        self.rcp_embedding = nn.Embedding(num_recipes, embedding_dim)
 
         nn.init.normal_(self.usr_embedding.weight, std=0.1)
-        nn.init.normal_(self.rec_embedding.weight, std=0.1)
+        nn.init.normal_(self.rcp_embedding.weight, std=0.1)
 
         self.alpha = 1 / (num_layers + 1)
         self.conv = HeteroLGConv()
@@ -32,21 +32,22 @@ class HeteroLGN(nn.Module):
     def forward(
         self,
         edge_index_dict: dict[tuple[str, str, str], torch.Tensor],
+        **kwargs,
     ) -> tuple[torch.Tensor, torch.Tensor]:
         usr_x = self.usr_embedding.weight
         usr_out = usr_x * self.alpha
-        rec_x = self.rec_embedding.weight
-        rec_out = rec_x * self.alpha
+        rcp_x = self.rcp_embedding.weight
+        rec_out = rcp_x * self.alpha
 
         for _ in range(self.num_layers):
             # Calculate new recipe embeddings
-            usr_rec_edges = edge_index_dict[("user", "reviews", "recipe")]
-            rec_x = self.conv((usr_x, rec_x), usr_rec_edges)
-            rec_out += rec_x * self.alpha
+            usr_rcp_edges = edge_index_dict[("user", "reviews", "recipe")]
+            rcp_x = self.conv((usr_x, rcp_x), usr_rcp_edges)
+            rec_out += rcp_x * self.alpha
 
             # Calculate new user embeddings
-            rec_usr_edges = edge_index_dict[("recipe", "rev_reviews", "user")]
-            usr_x = self.conv((rec_x, usr_x), rec_usr_edges)
+            rcp_usr_edges = edge_index_dict[("recipe", "rev_reviews", "user")]
+            usr_x = self.conv((rcp_x, usr_x), rcp_usr_edges)
             usr_out += usr_x * self.alpha
 
         return usr_out, rec_out
@@ -64,10 +65,9 @@ class HeteroLGN(nn.Module):
         neg_x = (rec_out[neg_rcp_idx] * usr_out[src_usr_idx]).sum(dim=-1)
 
         log_prob = F.logsigmoid(pos_x - neg_x).mean()
-        # reg_loss = self.lambda_val * (
-        #     self.usr_embedding(src_usr_idx).norm(2).pow(2)
-        #     + self.rec_embedding(pos_rcp_idx).norm(2).pow(2)
-        #     + self.rec_embedding(neg_rcp_idx).norm(2).pow(2)
-        # )
-        return -log_prob
-        # return -log_prob + reg_loss
+        reg_loss = self.lambda_val * (
+            self.usr_embedding(src_usr_idx).norm(2).pow(2)
+            + self.rcp_embedding(pos_rcp_idx).norm(2).pow(2)
+            + self.rcp_embedding(neg_rcp_idx).norm(2).pow(2)
+        )
+        return -log_prob + reg_loss
